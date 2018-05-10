77ff3c4b4391d356bcca612c22bc5ef230d36e5b
RakaSharmaGitHub <39092425+RakaSharmaGitHub@users.noreply.github.com>
2018-05-10 10:55:35 +0100

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,5 +36,17 @@ namespace GradeBook.GradeBooks
                 return 'F';
 
         }
+
+        public override void CalculateStatistics()
+        {
+
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
     }
 }
